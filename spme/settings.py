25f9ba53cf31aa997f503a846a1f02f361d757ff
marcelo <MRolqueza@gmail.com>
2025-07-29 21:08:39 -0400

    CORS configurado

--- spme/settings.py
+++ spme/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'rest_framework_simplejwt',
+    'corsheaders',
     'spme_web',
     'spme_api',
     'spme_autenticacion',
@@ -49,6 +50,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -167,3 +169,7 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'spme_autenticacion.Usuario'
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173", 
+]
