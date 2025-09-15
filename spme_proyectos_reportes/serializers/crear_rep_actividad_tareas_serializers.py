f24cbffe75ce20c068b45033acfda42e12777ef7
marcelo <MRolqueza@gmail.com>
2025-09-15 15:57:32 -0400

    Reportes de actividad relacionados

---
+++ spme_proyectos_reportes/serializers/crear_rep_actividad_tareas_serializers.py
@@ -0,0 +1,51 @@
+from rest_framework import serializers
+from spme_actividades.models import Actividad, TareaActividad
+from spme_monitoreo.models import *
+
+class TareaActividadSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = TareaActividad
+        fields = '__all__'
+
+class SolicitudFondosSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = SolicitudFondos
+        fields = '__all__'
+
+class SolicitudReembolsoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = SolicitudReembolso
+        fields = '__all__'
+
+class SolicitudPagoDirectoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = SolicitudPagoDirecto
+        fields = '__all__'
+
+class SolicitudViajeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = SolicitudViaje
+        fields = '__all__'
+
+class RendicionCuentasSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = RendicionCuentas
+        fields = '__all__'
+
+class InformeActividadSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = InformeActividad
+        fields = '__all__'
+
+class ActividadReporteSerializer(serializers.ModelSerializer):
+    tareas = TareaActividadSerializer(many=True, read_only=True)
+    solicitud_fondos = SolicitudFondosSerializer(many=True, read_only=True, source='usuario_actividad_solicitud')
+    solicitud_reembolso = SolicitudReembolsoSerializer(many=True, read_only=True, source='usuario_actividad_reembolso')
+    solicitud_pago_directo = SolicitudPagoDirectoSerializer(many=True, read_only=True, source='usuario_actividad_sol_pago_directo')
+    solicitud_viaje = SolicitudViajeSerializer(many=True, read_only=True, source='usuario_actividad_sol_viaje')
+    rendicion_cuentas = RendicionCuentasSerializer(many=True, read_only=True, source='rendicion_cuentas_actividad')
+    informe_actividad = InformeActividadSerializer(many=True, read_only=True, source='actividad_informe_actividad')
+    
+    class Meta:
+        model = Actividad
+        fields = '__all__'
